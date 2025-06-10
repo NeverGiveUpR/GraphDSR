216b964de03596f3115de19d620a3c152497b86d
Jingyi Liu <133211473+NeverGiveUpR@users.noreply.github.com>
2025-06-10 09:53:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,14 @@
 import argparse
-import numpy as np
-import pandas as pd
-import json
 from operators import Operators
 from Graph import ExprGraph
 from Models import GNN
-# from test import *
-import torch as th
-from train import train
+from new_train import train
 import Equations as Eq
 import FeynmanEquations as Fq
-import ReusableEquations as RE
+
 import warnings
 warnings.filterwarnings("ignore")
-
-
-def load_defaults_config():
-    """
-    Load defaults for training args.
-    """
-    with open('./config.json', 'r') as f:
-        return json.load(f)
-
-def add_dict_to_argparser(parser, default_dict):
-    for k, v in default_dict.items():
-        v_type = type(v)
-        if v is None:
-            v_type = str
-        elif isinstance(v, bool):
-            v_type = str2bool
-        parser.add_argument(f"--{k}", default=v, type=v_type)
-    return parser
-
-def str2bool(v):
-    """
-    https://stackoverflow.com/questions/15008758/parsing-boolean-values-with-argparse
-    """
-    if isinstance(v, bool):
-        return v
-    if v.lower() in ("yes", "true", "t", "y", "1"):
-        return True
-    elif v.lower() in ("no", "false", "f", "n", "0"):
-        return False
-    else:
-        raise argparse.ArgumentTypeError("boolean value expected")
+from utils import load_defaults_config, add_dict_to_argparser
 
 def main():
     # loading hyperparameters
@@ -51,47 +16,41 @@ def main():
     defaults.update(load_defaults_config())
     parser = argparse.ArgumentParser(description='training args.')
     parser = add_dict_to_argparser(parser, defaults)
+    parser.add_argument('--dataset', type=str, default='RE', help='name of the dataset, \'Eq\' for benchmarks, \'Fq\' for AI Feynman')
+    parser.add_argument('--expr', type=str, default='nguyen1', help='name of the expression')
     args = parser.parse_args()
+    dataset = args.dataset
+    expr = args.expr
+    print("dataset:{}, expr:{}".format(dataset, expr))
 
-    # exprs = list(Eq.func_dict.keys())[57:60]
-    # exprs = list(Fq.s_func_dict.keys())[19:25]
-    exprs = list(RE.func_dict.keys())[2:6]
-    exprs = ['var2_11']
-    results = []
-    for expr in exprs:
-        a_expr = []
-        for i in range(1):
-            # X, y = Eq.func_dict[expr]()
-            X, y = RE.func_dict[expr](n=100)
-            # try:
-            #     X, y = Fq.s_func_dict[expr](n=100)
-            # except:
-            #     X, y = Fq.s_func_dict[expr](num=100)
-            var_num = X.shape[1]
-            node_num = args.node_num + var_num
-            args.var_number = var_num
-            operators = Operators(args.operator_list, args.var_number)
-            gnn = GNN(args.pass_steps, node_num, 
-                    len(operators.library), args.hidden_size)
+    
+    if dataset == 'Fq':
+        try:
+            X, y = Fq.s_func_dict[expr](n=100)
+        except:
+            X, y = Fq.s_func_dict[expr](num=100)
+    elif dataset == 'Eq':
+        X, y = Eq.func_dict[expr]()
+    else:
+        raise TypeError("No such dataset!")
+    var_num = X.shape[1]
+    node_num = args.node_num + var_num
+    args.var_number = var_num
+    operators = Operators(args.operator_list, args.var_number)
+    gnn = GNN(args.pass_steps, node_num, 
+            len(operators.library), args.hidden_size)
 
-            graphs = ExprGraph(node_num=node_num, operators=operators, 
-                            args=args, additional_cnst=True)
-            
-            print("_______________________ {}{} ___________________________".format("sampling ", expr))
-            global_opt, time_cst = train(graphs, 
-                                         gnn, 
-                                         operators,
-                                         args, X=X, y=y,
-                                         expr=expr)
-            print("global_opt:", global_opt)
-            print("Spends {}s".format(time_cst))
-            a_expr.append([expr, 
-                           global_opt['reward'],
-                           global_opt['expr'],
-                           global_opt['complex'],
-                           time_cst])
-            df = pd.DataFrame(a_expr, columns=['dataset', 'reward', 'expr', 'complex','time'])
-            df.to_csv('./results/'+expr+'.csv')
+    graphs = ExprGraph(node_num=node_num, operators=operators, 
+                    args=args, additional_cnst=True)
+    
+    print("_______________________ {}{} ___________________________".format("sampling ", expr))
+    global_opt, time_cst = train(graphs, 
+                                    gnn, 
+                                    operators,
+                                    args, X=X, y=y,
+                                    expr=expr)
+    print("global_opt:", global_opt)
+    print("Spends {}s".format(time_cst))
             
 
 if __name__=='__main__':
